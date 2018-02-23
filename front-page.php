9726f0e7d33fa75e916d8f925304108fb0152a70
TuanPham303 <phamcongtuan303@gmail.com>
2018-02-23 15:44:11 -0800

    fix bug where the_field return null

--- front-page.php
+++ front-page.php
@@ -25,11 +25,44 @@ get_header(); ?>
           <a href="<?php echo site_url('/portfolio'); ?>"><button id="viewwork" class="alternate">View Our Work</button></a>
         </div>
       </section>
+      <?php endwhile ?>
 
+
+     
+
+      <section>
+        <div class="max-width-wrapper">
+          <a href="<?php echo site_url('/portfolio'); ?>" class="align-center"><button>Featured Case Studies</button></a>
+          <div id="featured-case-studies">
+            <?php 
+            $elements = get_posts(
+              array(
+              'post_type' => 'clients',
+              )
+            );
+            foreach($elements as $element): ?>
+            <article class="post-portfolio">
+              <a href="<?php echo get_permalink($element) ?>"><img src="<?php echo get_the_post_thumbnail_url($element, 'large'); ?>"/></a>
+              <div class="post-meta">
+                <h2><?php echo $element->post_excerpt ?></h2>
+                <p>
+                  <?php $terms = get_the_terms($element , 'category'); ?>
+                  <?php foreach($terms as $term): ?>
+                  <span><?php echo $term->name ?></span>
+                  <?php endforeach?>
+                </p>
+              </div>
+            </article>
+            <?php endforeach ?>
+          </div>
+        </div>
+      </section>
+
+      <?php while(have_posts()): the_post(); ?>
       <section class="pd-none">
         <div id="accordion-slider" data-index="0">
           <?php foreach(range(1,4) as $number): ?>
-            <figure class="<?php if($number==1){echo 'state-active';} ?>">
+            <figure class="<?php if($number == 1){echo 'state-active';} ?>">
               <div class="max-width-wrapper">
                 <img src="<?php the_field('slide_gif_' . $number); ?>"/>
                 <?php if(get_field('slide_gif_' . $number . '_desktop')): ?><img src="<?php the_field('slide_gif_' . $number . '_desktop'); ?>"/><?php endif ?>
@@ -49,39 +82,7 @@ get_header(); ?>
         </menu>
 
       </section>
-
       <?php endwhile ?>
-
-      <?php
-      $posts = get_posts(
-        array(
-        'post_type' => 'clients',
-        'numberposts' => 3
-        )
-      );
-      ?>
-
-      <section>
-        <div class="max-width-wrapper">
-          <a href="<?php echo site_url('/portfolio'); ?>" class="align-center"><button>Featured Case Studies</button></a>
-          <div id="featured-case-studies">
-            <?php foreach($posts as $post): ?>
-            <article class="post-portfolio">
-              <a href="<?php echo get_permalink($post) ?>"><img src="<?php echo get_the_post_thumbnail_url($post, 'large'); ?>"/></a>
-              <div class="post-meta">
-                <h2><?php echo $post->post_excerpt ?></h2>
-                <p>
-                  <?php $terms = get_the_terms($post , 'category'); ?>
-                  <?php foreach($terms as $term): ?>
-                  <span><?php echo $term->name ?></span>
-                  <?php endforeach?>
-                </p>
-              </div>
-            </article>
-            <?php endforeach ?>
-          </div>
-        </div>
-      </section>
 
     </main><!-- #main -->
   </div><!-- #primary -->
