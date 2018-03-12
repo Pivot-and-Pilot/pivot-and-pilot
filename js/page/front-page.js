923f0f3e38ebd900ddd26903d3d18fe128bada66
TuanPham303 <phamcongtuan303@gmail.com>
2018-03-12 11:57:49 -0700

    close tab when open other tab

--- js/page/front-page.js
+++ js/page/front-page.js
@@ -254,28 +254,32 @@ jQuery(document).ready(function($){
         $('.how-we-do-it-title').on('click', (e) => {
           if ($(window).width() < 1050) {
             if ( $($(e.target).parent()).css('height') == '65px' ) {
+							$('.how-we-do-it-single-wrap').css('max-height', '65px');
+							$('.line-two').css('display', 'block');
               $($(e.target).parent()).css({
                 'max-height': '2000px',
               });
-              $($(e.target).children().children()[1]).toggle();
+              $($(e.target).children().children()[1]).css('display', 'none');
             } else {
               $($(e.target).parent()).css({
                 'max-height': '65px',
               });
-              $($(e.target).children().children()[1]).toggle();
+              $($(e.target).children().children()[1]).css('display', 'block');
             }
           }
           if ($(window).width() > 1050) {
             if ( $($(e.target).parent()).css('height') == '80px' ) {
+							$('.how-we-do-it-single-wrap').css('max-height', '80px');
+							$('.line-two').css('display', 'block');
               $($(e.target).parent()).css({
                 'max-height': '2000px',
               });
-              $($(e.target).children().children()[1]).toggle();
+              $($(e.target).children().children()[1]).css('display', 'none');
             } else {
               $($(e.target).parent()).css({
                 'max-height': '80px',
               });
-              $($(e.target).children().children()[1]).toggle();
+              $($(e.target).children().children()[1]).css('display', 'block');
             }
           }
         })
